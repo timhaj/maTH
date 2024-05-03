21257d99c80af9634da8aee6abd5eaa6efbaebd9
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-05-03 12:44:40 +0200

    Added pascal triangle, some combinatorics equations and rewrote some functions into template functions for strict type checking

--- maTH.cpp
+++ maTH.cpp
@@ -1,15 +1,24 @@
 #include "maTH.h"
+#include <vector>
 
-int add(int a, int b) {
-    return a + b;
+template<typename ReturnType, typename T1, typename T2>
+ReturnType add(T1 a, T2 b) {
+    return (ReturnType) a + (ReturnType) b;
 }
 
-int subtract(int a, int b) {
-    return a - b;
+template<typename ReturnType, typename T1, typename T2>
+ReturnType subtract(T1 a, T2 b) {
+    return (ReturnType) a - (ReturnType) b;
 }
 
-int multiply(int a, int b){
-    return a * b;
+template<typename ReturnType, typename T1, typename T2>
+ReturnType multiply(T1 a, T2 b) {
+    return (ReturnType) a * (ReturnType) b;
+}
+
+template<typename ReturnType, typename T1, typename T2>
+ReturnType divide(T1 a, T2 b){
+    return (ReturnType) a / (ReturnType) b;
 }
 
 int factorial_rec(int n){
@@ -25,5 +34,39 @@ int factorial(int n){
         res *= i;
     }
     return res;
+}
+
+std::vector<int> pascal(int n){
+    if(n == 1){
+        return {1};
+    }
+    else if(n == 2){
+        return {1,1};
+    }
+    else{
+        std::vector<int> arr(2,1); // {1,1}
+        for(int i = 0;i<n-2;i++){
+            std::vector<int> tmp(arr.size()+1);
+            tmp[0] = 1;
+            tmp[arr.size()] = 1;
+            for(int j = 0;j<arr.size()-1;j++){
+                tmp[j+1] = arr[j] + arr[j+1];
+            }
+            arr.swap(tmp);
+        }
+        return arr;
+    }
+}
+
+int combinations(int n, int k){
+    return factorial(n)/(factorial(k)*factorial(n-k));
+}
+
+int combinationsWithRep(int n, int k){
+    return combinations(n+k-1,k);
+}
+
+int permutations(int n, int k){
+    return factorial(n)/factorial(n-k);
 }
 
--- maTH.h
+++ maTH.h
@@ -1,10 +1,17 @@
 #ifndef MATH_H
 #define MATH_H
 
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
+template<typename ReturnType, typename T1, typename T2>
+ReturnType add(T1 a, T2 b);
+template<typename ReturnType, typename T1, typename T2>
+ReturnType subtract(T1 a, T2 b);
+template<typename ReturnType, typename T1, typename T2>
+ReturnType multiply(T1 a, T2 b);
+template<typename ReturnType, typename T1, typename T2>
+ReturnType divide(T1 a, T2 b);
 int factorial(int n);
 int factorial_rec(int n);
+std::vector<int> pascal(int n);
+int combinations(int n, int k);
 
 #endif
