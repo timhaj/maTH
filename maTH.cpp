0f7944f950d6b81f7ff9d8fc962e3039d6b88972
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-05-03 17:49:28 +0200

    Added sine and cosine approximation functions

--- maTH.cpp
+++ maTH.cpp
@@ -1,56 +1,71 @@
 #include "maTH.h"
 #include <vector>
+#include <cmath>
 
-template<typename ReturnType, typename T1, typename T2>
-ReturnType add(T1 a, T2 b) {
-    return (ReturnType) a + (ReturnType) b;
+template <typename ReturnType, typename T1, typename T2>
+ReturnType add(T1 a, T2 b)
+{
+    return (ReturnType)a + (ReturnType)b;
 }
 
-template<typename ReturnType, typename T1, typename T2>
-ReturnType subtract(T1 a, T2 b) {
-    return (ReturnType) a - (ReturnType) b;
+template <typename ReturnType, typename T1, typename T2>
+ReturnType subtract(T1 a, T2 b)
+{
+    return (ReturnType)a - (ReturnType)b;
 }
 
-template<typename ReturnType, typename T1, typename T2>
-ReturnType multiply(T1 a, T2 b) {
-    return (ReturnType) a * (ReturnType) b;
+template <typename ReturnType, typename T1, typename T2>
+ReturnType multiply(T1 a, T2 b)
+{
+    return (ReturnType)a * (ReturnType)b;
 }
 
-template<typename ReturnType, typename T1, typename T2>
-ReturnType divide(T1 a, T2 b){
-    return (ReturnType) a / (ReturnType) b;
+template <typename ReturnType, typename T1, typename T2>
+ReturnType divide(T1 a, T2 b)
+{
+    return (ReturnType)a / (ReturnType)b;
 }
 
-int factorial_rec(int n){
-    if(n == 1 || n == 0){
+int factorial_rec(int n)
+{
+    if (n == 1 || n == 0)
+    {
         return 1;
     }
-    return n*factorial(n-1);
+    return n * factorial(n - 1);
 }
 
-int factorial(int n){
+int factorial(int n)
+{
     int res = 1;
-    for(int i = 1;i<=n;i++){
+    for (int i = 1; i <= n; i++)
+    {
         res *= i;
     }
     return res;
 }
 
-std::vector<int> pascal(int n){
-    if(n == 1){
+std::vector<int> pascal(int n)
+{
+    if (n == 1)
+    {
         return {1};
     }
-    else if(n == 2){
-        return {1,1};
+    else if (n == 2)
+    {
+        return {1, 1};
     }
-    else{
-        std::vector<int> arr(2,1); // {1,1}
-        for(int i = 0;i<n-2;i++){
-            std::vector<int> tmp(arr.size()+1);
+    else
+    {
+        std::vector<int> arr(2, 1); // {1,1}
+        for (int i = 0; i < n - 2; i++)
+        {
+            std::vector<int> tmp(arr.size() + 1);
             tmp[0] = 1;
             tmp[arr.size()] = 1;
-            for(int j = 0;j<arr.size()-1;j++){
-                tmp[j+1] = arr[j] + arr[j+1];
+            for (int j = 0; j < arr.size() - 1; j++)
+            {
+                tmp[j + 1] = arr[j] + arr[j + 1];
             }
             arr.swap(tmp);
         }
@@ -58,15 +73,69 @@ std::vector<int> pascal(int n){
     }
 }
 
-int combinations(int n, int k){
-    return factorial(n)/(factorial(k)*factorial(n-k));
+int combinations(int n, int k)
+{
+    return factorial(n) / (factorial(k) * factorial(n - k));
 }
 
-int combinationsWithRep(int n, int k){
-    return combinations(n+k-1,k);
+int combinationsWithRep(int n, int k)
+{
+    return combinations(n + k - 1, k);
 }
 
-int permutations(int n, int k){
-    return factorial(n)/factorial(n-k);
+int permutations(int n, int k)
+{
+    return factorial(n) / factorial(n - k);
 }
 
+template <typename returnType = double, typename T>
+returnType sin(T x, int terms = 10)
+{
+    x = fmod(x, PI); // Reduce x to the range of [-2pi, 2pi]
+    if (x < -PI)
+    {
+        x += 2 * PI; // Adjust x to the range of [-pi, pi]
+    }
+    else if (x > PI)
+    {
+        x -= 2 * PI; // Adjust x to the range of [-pi, pi]
+    }
+    returnType result = 0;
+    returnType term = x;
+    returnType denom = 1;
+    returnType sign = 1;
+    for (int i = 1; i <= terms; ++i)
+    {
+        result += sign * (term / factorial(denom));
+        term *= (x * x);
+        denom += 2;
+        sign *= -1;
+    }
+    return result;
+}
+
+template <typename returnType = double, typename T>
+returnType cos(T x, int terms = 10)
+{
+    x = fmod(x, 2 * PI); // Reduce x to the range of [-2pi, 2pi]
+    if (x < -PI)
+    {
+        x += 2 * PI; // Adjust x to the range of [-pi, pi]
+    }
+    else if (x > PI)
+    {
+        x -= 2 * PI; // Adjust x to the range of [-pi, pi]
+    }
+    returnType result = 0;
+    returnType term = 1;
+    returnType denom = 0;
+    returnType sign = 1;
+    for (int i = 0; i < terms; ++i)
+    {
+        result += sign * (term / factorial(denom));
+        term *= (x * x);
+        denom += 2;
+        sign *= -1;
+    }
+    return result;
+}
--- maTH.h
+++ maTH.h
@@ -1,17 +1,35 @@
+#include <vector>
 #ifndef MATH_H
 #define MATH_H
 
+constexpr long double PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+constexpr long double E = 2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274;
+constexpr long double PHI = 1.61803398874989484820458683436563811772030917980576286213544862270526046281890;
+constexpr long double TAU = 0.61803398874989484820458683436563811772030917980576286213544862270526046281890;
+
 template<typename ReturnType, typename T1, typename T2>
 ReturnType add(T1 a, T2 b);
+
 template<typename ReturnType, typename T1, typename T2>
 ReturnType subtract(T1 a, T2 b);
+
 template<typename ReturnType, typename T1, typename T2>
 ReturnType multiply(T1 a, T2 b);
+
 template<typename ReturnType, typename T1, typename T2>
 ReturnType divide(T1 a, T2 b);
+
 int factorial(int n);
 int factorial_rec(int n);
 std::vector<int> pascal(int n);
 int combinations(int n, int k);
+int combinationsWithRep(int n, int k);
+int permutations(int n, int k);
+
+template <typename returnType = double, typename T>
+returnType sin(T x, int terms = 10);
+
+template <typename returnType = double, typename T>
+returnType cos(T x, int terms = 10);
 
 #endif
