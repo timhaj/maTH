ba0275c1dbd692d152b0da13f6bc718a5e624b6c
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-05-07 18:15:35 +0200

    Deleted some files

--- updaters/updateLib.bat
+++
@@ -1,3 +0,0 @@
-g++ -c maTH.cpp -o maTH.o
-ar rcs libmaTH.a maTH.o
-g++ main.cpp -L. -lmaTH -o main_executable
--- updaters/updateLib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-g++ -c maTH.cpp -o maTH.o
-ar rcs libmaTH.a maTH.o
-g++ main.cpp -L. -lmaTH -o main_executable
