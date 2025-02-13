4b878eb88f050492267fae1a0058c96037ef4943
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2025-02-13 22:23:26 +0100

    Started adding algorithms -> no better start with the good old bubble sort

--- maTH.h
+++ maTH.h
@@ -637,4 +637,18 @@ struct Uniform {
     }     
 };
 
+template<typename T>
+void bubbleSort(std::vector<T>& arr, bool isAscending){
+    for(int i = 0;i<arr.size();i++){
+        for(int j = 0;j<arr.size()-i-1;j++){
+            if(((arr[j] < arr[j+1]) && !isAscending) || ((arr[j] > arr[j+1]) && isAscending)){
+                T tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+}
+
+
 #endif
