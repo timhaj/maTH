e4e11ea26b5d1a9e09d45bf6042fea442e52aa16
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2025-02-15 22:39:46 +0100

    Merge sort

--- maTH.h
+++ maTH.h
@@ -678,4 +678,57 @@ void selectionSort(std::vector<T>& arr, bool isAscending){
     }
 }
 
+template<typename T>
+void merge(std::vector<T>& arr, int left, int mid, int right, bool isAscending) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    
+    std::vector<T> L(n1);
+    std::vector<T> R(n2);
+    
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+    
+    int i = 0, j = 0, k = left;
+    while(i < n1 && j < n2) {
+        if((L[i] <= R[j] && isAscending) || (L[i] >= R[j] && !isAscending)) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while(i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while(j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+template<typename T>
+void mergeSort(std::vector<T>& arr, int left, int right, bool isAscending) {
+    if(left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid, isAscending);
+        mergeSort(arr, mid + 1, right, isAscending);
+        merge(arr, left, mid, right, isAscending);
+    }
+}
+
+template<typename T>
+void mergeSort(std::vector<T>& arr, bool isAscending) {
+    mergeSort(arr, 0, arr.size() - 1, isAscending);
+}
+
 #endif
