2f97485e90b480278ec0db563151e0729e2b6c13
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2025-02-17 20:10:24 +0100

    Radix sort

--- maTH.h
+++ maTH.h
@@ -890,4 +890,46 @@ void bucketSort(std::vector<T>& arr, bool isAscending) {
     insertionSort(arr, isAscending);
 }
 
+void countingSortByDigit(std::vector<int>& arr, int exp, bool isAscending) {
+    std::vector<int> output(arr.size());
+    std::vector<int> count(10, 0);
+
+    for (int i = 0; i < arr.size(); i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    if (isAscending) {
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = output[i];
+        }
+    } 
+    else {
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = output[arr.size() - 1 - i];
+        }
+    }
+}
+
+void radixSort(std::vector<int>& arr, bool isAscending) {
+    int maxVal = arr[0];
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+
+    for (int exp = 1; maxVal / exp > 0; exp *= 10) {
+        countingSortByDigit(arr, exp, isAscending);
+    }
+}
+
 #endif
